74760b0781eadafbf7f94f77506bbcd6fc04b52e
MichalRsa <michalrosa17@gmail.com>
2024-04-05 07:44:39 +0200

    Clean up getComponents.js and getStories.js

--- .scripts/utils/getComponents.js
+++ .scripts/utils/getComponents.js
@@ -1,44 +1,41 @@
 import fs from "fs";
+import StoryblokService from "../services/StoryblokService.js";
 
-const spaceId = process.env.STORYBLOK_SPACE_ID;
+export const getComponents = async () => {
+  try {
+    const response = await StoryblokService.getComponents();
 
-export const getComponents = async (StoryblokClient) => {
-  await StoryblokClient.get(`/spaces/${spaceId}/components/`, {})
-    .then((response) => {
-      const componentGroups = response.data.component_groups;
+    const componentGroups = response.data.component_groups;
 
-      try {
-        const components = response.data.components.map((component) => {
-          return {
-            ...component,
-            groupName: componentGroups.find(
-              (group) => group.uuid === component.component_group_uuid,
-            ).name,
-          };
-        });
-
-        fs.writeFile(
-          "exportedData/component_group.json",
-          JSON.stringify(componentGroups, null, 2),
-          (err) => {
-            if (err) throw err;
-            console.log("Data written to file");
-          },
-        );
-
-        fs.writeFile(
-          "exportedData/components.json",
-          JSON.stringify(components, null, 2),
-          (err) => {
-            if (err) throw err;
-            console.log("Data written to file");
-          },
-        );
-      } catch (error) {
-        console.error("Error parsing JSON:", error.message);
-      }
-    })
-    .catch((error) => {
-      console.log(error);
+    const components = response.data.components.map((component) => {
+      return {
+        ...component,
+        groupName: componentGroups.find(
+          (group) => group.uuid === component.component_group_uuid,
+        ).name,
+      };
     });
+
+    fs.writeFile(
+      "exportedData/component_group.json",
+      JSON.stringify(componentGroups, null, 2),
+      (err) => {
+        if (err)
+          console.log("Error while writting to component_group.json file", err);
+        console.log("Data written to file - component_group.json");
+      },
+    );
+
+    fs.writeFile(
+      "exportedData/components.json",
+      JSON.stringify(components, null, 2),
+      (err) => {
+        if (err)
+          console.log("Error while writting to component.json file", err);
+        console.log("Data written to file - components.json");
+      },
+    );
+  } catch (error) {
+    console.error("Error while fetching components", error);
+  }
 };
--- .scripts/utils/getStories.js
+++ .scripts/utils/getStories.js
@@ -1,31 +1,29 @@
 import fs from "fs";
+import StoryblokService from "../services/StoryblokService.js";
 
-const spaceId = process.env.STORYBLOK_SPACE_ID;
+export const getStories = async () => {
+  try {
+    const response = await StoryblokService.getStories();
+    const storiesId = response.data.stories.map((story) => story.id);
 
-export const getStories = async (StoryblokClient) => {
-  let storiesId;
-  await StoryblokClient.get(`/spaces/${spaceId}/stories/`, {})
-    .then((response) => {
-      storiesId = response.data.stories.map((story) => story.id);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    storiesId.forEach(async (id) => {
+      const response = await StoryblokService.getStory(id);
+      const name = response.data.story.slug;
+      const isFolder = response.data.story.is_folder;
 
-  storiesId.forEach(async (id) => {
-    StoryblokClient.get(`/spaces/${spaceId}/stories/${id}`, {})
-      .then((response) => {
-        const name = response.data.story.slug;
-        const isFolder = response.data.story.is_folder;
+      const fileName = isFolder ? `folder_${name}` : name;
 
-        const fileName = isFolder ? `folder_${name}` : name;
+      try {
         fs.writeFileSync(
           `exportedData/stories/${fileName}.json`,
           JSON.stringify(response.data.story, null, 2),
         );
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
+        console.log(`Data written to file - ${fileName}.json`);
+      } catch (error) {
+        console.log(`Error while writting ${fileName}`, error);
+      }
+    });
+  } catch (error) {
+    console.log("Error while fetching stories data", error);
+  }
 };
