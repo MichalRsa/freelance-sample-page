3558aea963799049708707a3118e4995d52c0d61
MichalRsa <michalrosa17@gmail.com>
2024-04-08 14:53:50 +0200

    Cleans up exportSpaceData.js and importSpaceData.js

--- .scripts/exportSpaceData.js
+++ .scripts/exportSpaceData.js
@@ -1,12 +1,6 @@
-import StoryblokClient from "storyblok-js-client";
 import { getComponents } from "./utils/getComponents.js";
 import { getStories } from "./utils/getStories.js";
-const userToken = process.env.STORYBLOK_USER_AUTH_TOKEN;
 
-const Storyblok = new StoryblokClient({
-  oauthToken: userToken,
-});
+getComponents();
 
-getComponents(Storyblok);
-
-getStories(Storyblok);
+getStories();
--- .scripts/importSpaceData.js
+++ .scripts/importSpaceData.js
@@ -1,13 +1,6 @@
-import StoryblokClient from "storyblok-js-client";
 import { pushComponents } from "./utils/pushComponents.js";
 import { pushStories } from "./utils/pushStories.js";
 
-const userToken = process.env.DESTINATION_STORYBLOK_USER_AUTH_TOKEN;
+pushComponents();
 
-const Storyblok = new StoryblokClient({
-  oauthToken: userToken,
-});
-
-pushComponents(Storyblok);
-
-pushStories(Storyblok);
+pushStories();
