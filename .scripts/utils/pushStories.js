1943dd87a85f07275b171e530643eb58b577013a
MichalRsa <michalrosa17@gmail.com>
2024-04-08 10:28:19 +0200

    Cleans up pushStories.js file

--- .scripts/utils/pushStories.js
+++ .scripts/utils/pushStories.js
@@ -1,43 +1,25 @@
 import fs from "fs";
+import StoryblokService from "../services/StoryblokService.js";
 
-const spaceId = process.env.DESTINATION_STORYBLOK_SPACE_ID;
-
-export const pushStories = (StoryblokClient) => {
+export const pushStories = () => {
   fs.readdir("exportedData/stories/", async (err, files) => {
     if (err) {
       console.error("Error reading directory:", err);
       return;
     }
 
-    const rootLevelStories = [];
+    const stories = [];
     const folders = [];
-    const nestedStories = [];
 
     files.forEach((file) => {
       const story = JSON.parse(
         fs.readFileSync(`exportedData/stories/${file}`, "utf8"),
       );
+
       if (story.is_folder) {
         folders.push(story);
-      } else if (!!story.parent_id) {
-        nestedStories.push(story);
-      } else rootLevelStories.push(story);
+      } else stories.push(story);
     });
-
-    await Promise.all(
-      rootLevelStories.map(async (story) => {
-        await StoryblokClient.post(`/spaces/${spaceId}/stories`, {
-          story: { ...story },
-        })
-          .then((response) => {
-            return response;
-          })
-          .catch((error) => {
-            console.log(`Error while adding story - ${story.name}`);
-            console.log(error);
-          });
-      }),
-    );
 
     const pushedFoldersIds = [];
 
@@ -47,9 +29,7 @@ export const pushStories = (StoryblokClient) => {
       !!idsArray.find((id) => id.oldId === parentId);
 
     const pushFolders = async (folders) => {
-      count++;
-      if (count >= 20)
-        return console.log("We have infinite loop! STOP NOW!!!!!!!");
+      if (count >= 20) return console.log("Too manu requests");
 
       if (folders.length === 0)
         return console.log("All folders has been added");
@@ -67,19 +47,17 @@ export const pushStories = (StoryblokClient) => {
                 .newId
             : folder.parent_id;
           try {
-            const res = await StoryblokClient.post(
-              `/spaces/${spaceId}/stories`,
-              {
-                story: { ...folder },
-              },
-            );
+            const res = await StoryblokService.postStory(folder);
 
             const pushedFolder = res.data.story;
 
             pushedFoldersIds.push({ newId: pushedFolder.id, oldId: folder.id });
 
+            count++;
             return pushedFolder;
-          } catch (error) {}
+          } catch (error) {
+            console.log("Error while adding folder", error);
+          }
         });
       }, Promise.resolve());
 
@@ -89,16 +67,14 @@ export const pushStories = (StoryblokClient) => {
     await pushFolders(folders);
 
     await Promise.all(
-      nestedStories.map(async (story) => {
+      stories.map(async (story) => {
         const isParent = doesParentExist(story.parent_id, pushedFoldersIds);
 
         story.parent_id = isParent
           ? pushedFoldersIds.find((ids) => (ids.oldId = story.parent_id)).newId
           : story.parent_id;
 
-        await StoryblokClient.post(`/spaces/${spaceId}/stories`, {
-          story: { ...story },
-        }).catch((error) => {
+        await StoryblokService.postStory(story).catch((error) => {
           console.log(`Error while adding story - ${story.name}`);
           console.log(error);
         });
